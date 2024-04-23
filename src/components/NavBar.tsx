eb0c0fd887574b936dfe6f0b159c33e2843b27c0
Beau Findlay <me@beaufindlay.com>
2024-04-23 17:01:22 +0100

    Fix nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColourModeSwitch from "./ColourModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="4rem" />
+      <Link to="/">
+        <Image src={logo} boxSize="4rem" objectFit="cover" />
+      </Link>
       <SearchInput />
       <ColourModeSwitch />
     </HStack>
