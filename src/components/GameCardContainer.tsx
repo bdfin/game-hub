bd4b597089cd03b52610666ce1db720516c7086f
Beau Findlay <me@beaufindlay.com>
2024-04-23 13:05:25 +0100

    Get game details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,14 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius="1rem" overflow="hidden">
+    <Box
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in ",
+      }}
+      borderRadius="1rem"
+      overflow="hidden"
+    >
       {children}
     </Box>
   );
