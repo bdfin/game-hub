6e845320806317d1510e476089e1748aa15ac3cb
Beau Findlay <me@beaufindlay.com>
2024-04-23 16:35:41 +0100

    Add game trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/apiClient";
+import { Trailer } from "../entities/Trailer";
+import ms from "ms";
+
+export default function useTrailers(gameId: number) {
+  const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
+
+  return useQuery({
+    queryKey: ["trailers", gameId],
+    queryFn: apiClient.getAll,
+    staleTime: ms("24h"),
+  });
+}
